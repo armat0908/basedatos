f324282e33bd10f0dcd2f5476456f3f3e8d571c3
armat0908 <armat0908@gmail.com>
2015-06-29 22:43:20 -0500

    Update TiendaCatalogo.sql

--- TiendaCatalogo.sql
+++ TiendaCatalogo.sql
@@ -2,16 +2,16 @@ select *from productos;
 commit;
 
 
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(1,'Balon1',399.90,'Tamaño 4 piel',1,'Images/balon1.jpeg','nike');
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(2,'Balon2',299.90,'Tamaño 5 piel',1,'Images/balon2.jpg','adidas');
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(3,'Balon3',599.90,'Tamaño 3 piel',1,'Images/balon3.jpg','puma');
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(4,'Balon4',699.90,'Tamaño 4 piel',1,'Images/balon4.jpg','nike');
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(5,'Balon5',799.90,'Tamaño 4 piel',1,'Images/balon5.jpg','nike');
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(6,'Balon6',899.90,'Tamaño 5 piel',1,'Images/balon6.jpg','nike');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(1,'Balon1',399.90,'TamaÃ±o 4 piel',1,'Images/balon1.jpeg','nike');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(2,'Balon2',299.90,'TamaÃ±o 5 piel',1,'Images/balon2.jpg','adidas');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(3,'Balon3',599.90,'TamaÃ±o 3 piel',1,'Images/balon3.jpg','puma');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(4,'Balon4',699.90,'TamaÃ±o 4 piel',1,'Images/balon4.jpg','nike');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(5,'Balon5',799.90,'TamaÃ±o 4 piel',1,'Images/balon5.jpg','nike');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(6,'Balon6',899.90,'TamaÃ±o 5 piel',1,'Images/balon6.jpg','nike');
 
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(100,'Balon4',699.90,'Tamaño 4 piel',1,'Images/balon3.jpg','nike');
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(101,'Balon5',799.90,'Tamaño 4 piel',1,'Images/balon5.jpg','nike');
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(102,'Balon6',899.90,'Tamaño 5 piel',1,'Images/balon6.jpg','nike');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(100,'Balon4',699.90,'TamaÃ±o 4 piel',1,'Images/balon3.jpg','nike');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(101,'Balon5',799.90,'TamaÃ±o 4 piel',1,'Images/balon5.jpg','nike');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(102,'Balon6',899.90,'TamaÃ±o 5 piel',1,'Images/balon6.jpg','nike');
 
 commit;
 
@@ -45,16 +45,17 @@ SELECT * FROM categorias order by id_categorias;
 describe usuarios;
 select *from usuarios;
 
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(20,'maleta1',399.90,'Tamaño 4 piel',4,'Images/maleta1.jpg','nike');
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(21,'maleta2',299.90,'Tamaño 5 piel',4,'Images/maleta2.jpg','adidas');
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(22,'maleta3',599.90,'Tamaño mediano piel',4,'Images/maleta3.jpg','puma');
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(41,'maleta4',699.90,'Tamaño 4 piel',4,'Images/maleta5.jpg','nike');
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(51,'maleta5',799.90,'Tamaño 4 piel',4,'Images/maleta6.jpg','nike');
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(61,'maleta6',899.90,'Tamaño grande piel',4,'Images/maleta6.jpg','nike');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(20,'maleta1',399.90,'TamaÃ±o 4 piel',4,'Images/maleta1.jpg','nike');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(21,'maleta2',299.90,'TamaÃ±o 5 piel',4,'Images/maleta2.jpg','adidas');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(22,'maleta3',599.90,'TamaÃ±o mediano piel',4,'Images/maleta3.jpg','puma');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(41,'maleta4',699.90,'TamaÃ±o 4 piel',4,'Images/maleta5.jpg','nike');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(51,'maleta5',799.90,'TamaÃ±o 4 piel',4,'Images/maleta6.jpg','nike');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(61,'maleta6',899.90,'TamaÃ±o grande piel',4,'Images/maleta6.jpg','nike');
 
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(14,'playera2',399.90,'Tamaño 4 piel',3,'Images/playera2.png','nike');
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(15,'playera3',299.90,'Tamaño 5 piel',3,'Images/playera3.jpg','adidas');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(14,'playera2',399.90,'TamaÃ±o 4 piel',3,'Images/playera2.png','nike');
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(15,'playera3',299.90,'TamaÃ±o 5 piel',3,'Images/playera3.jpg','adidas');
 
 
-insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(100,'playera3',299.90,'Tamaño 5 piel',3,'Images/playera3.jpg','adidas');
-
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(100,'playera3',299.90,'TamaÃ±o 5 piel',3,'Images/playera3.jpg','adidas');
+------inserta otro producto----
+insert into productos(id_productos, nombre, precio, descripcion, id_categorias,imagen,marca) values(200,'playera1',199.90,' piel',3,'Images/playera1.jpg','adidas');
